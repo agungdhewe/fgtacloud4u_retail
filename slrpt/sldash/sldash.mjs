732f6e6b984c492e2af349e3da60395dcfe15d05
agungdhewe <agung_dhewe@yahoo.com>
2020-06-25 05:52:55 +0700

    update sl dashboard demo

--- slrpt/sldash/sldash.mjs
+++ slrpt/sldash/sldash.mjs
@@ -1,25 +1,28 @@
 
 
-var el_myChart = document.getElementById('myChart');
+var el_myChart_1 = document.getElementById('myChart-1');
+var el_myChart_2 = document.getElementById('myChart-2');
 
 
 
 export async function init() {
+	document.getElementsByTagName("body")[0].style.margin = '5px 5px 5px 5px'
 
-	CreateChart();
+	CreateChart_1();
+	CreateChart_2();
 }
 
 
-function CreateChart() {
-	var ctx = el_myChart.getContext('2d');
+function CreateChart_1() {
+	var ctx = el_myChart_1.getContext('2d');
 
 
 	var myChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			labels: ['JAN', 'FEB', 'MAR', 'APR', 'MEI', 'JUN'],
 			datasets: [{
-				label: '# of Votes',
+				label: 'Sales 2020',
 				backgroundColor: 'rgba(255, 99, 132, 0.2)',
 				data: [{x:'A', y:12}, {x:'B', y:19}, {x:'C', y:3}, {x:'D', y:5}, {x:'E', y:2}, {x:'F', y:3}],
 				// backgroundColor: [
@@ -59,4 +62,57 @@ function CreateChart() {
 			}			
 		}
 	});
+}
+
+
+
+
+function CreateChart_2() {
+
+	var randomScalingFactor = function() {
+		return Math.round(Math.random() * 100);
+	};
+
+
+	var ctx = el_myChart_2.getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'pie',
+		data: {
+			datasets: [{
+				data: [
+					randomScalingFactor(),
+					randomScalingFactor(),
+					randomScalingFactor(),
+					randomScalingFactor(),
+					randomScalingFactor(),
+				],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+				],
+				label: 'Dataset 1'
+			}],
+			labels: [
+				'Red',
+				'Orange',
+				'Yellow',
+				'Green',
+				'Blue'
+			]
+		},
+		options: {
+			responsive: true,
+			legend: {
+				position: 'right'
+			}
+		}
+	});
+
+
+
+
+
 }
